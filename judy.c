bb6887a57ec83f67b9f2795940ea157fbb8dccd9
arnimarj@gmail.com <arnimarj@gmail.com@28560f5c-fd42-11de-9d47-03d50c098576>
2010-07-15 11:13:54 +0000

    cleanup before iterator implementation.

--- judy.c
+++ judy.c
@@ -9,9 +9,7 @@ static PyMethodDef judy_methods[] =
 #define TRY_MODULE_ADD_OBJECT(module, key, value) if (PyModule_AddObject(module, key, value) != 0) return;
 
 extern PyTypeObject PyJudyIntObjectMapType;
-// extern PyTypeObject PyJudyIntObjectMapIterType;
-// extern PyTypeObject PyJudyIntSetType;
-// extern PyTypeObject PyJudyIntSetIterType;
+extern PyTypeObject PyJudyIntSetType;
 
 PyMODINIT_FUNC
 initjudy(void)
@@ -22,16 +20,11 @@ initjudy(void)
 		return;
 
 	if (PyType_Ready(&PyJudyIntSetType) < 0)
-			return;
-
-	if (PyType_Ready(&PyJudyIntSetIterType) < 0)
-			return;
+		return;
 
 	Py_INCREF(&PyJudyIntObjectMapType);
 	Py_INCREF(&PyJudyIntSetType);
-	Py_INCREF(&PyJudyIntSetIterType);
 
 	TRY_MODULE_ADD_OBJECT(judy_module, "JudyIntObjectMap", (PyObject*)&PyJudyIntObjectMapType);
 	TRY_MODULE_ADD_OBJECT(judy_module, "JudyIntSet",       (PyObject*)&PyJudyIntSetType);
-	TRY_MODULE_ADD_OBJECT(judy_module, "JudyIntSetIter",   (PyObject*)&PyJudyIntSetIterType);
 }
--- judy.h
+++ judy.h
@@ -11,31 +11,20 @@
 
 #include "utils.h"
 
-// #define JUDY_WORD_MAX (sizeof(Word_t) == 4 ? 0xFFFFFFFF : 0xFFFFFFFFFFFFFFFF)
-
 typedef struct {
 	PyObject_HEAD
 	Pvoid_t judy_L;
 } PyJudyIntObjectMap;
 
 typedef struct {
-        PyObject_HEAD
-		Pvoid_t s;
+	PyObject_HEAD
+	Pvoid_t s;
 } PyJudyIntSet;
-
-typedef struct {
-		PyObject_HEAD
-		int b;
-		Word_t i;
-		PyJudyIntSet* s;
-} PyJudyIntSetIter;
 
 extern PyTypeObject PyJudyIntObjectMapType;
 extern PyTypeObject PyJudyIntSetType;
-extern PyTypeObject PyJudyIntSetIterType;
 
 #define PyJudyIntObjectMap_Check(op) PyObject_TypeCheck(op, &PyJudyIntObjectMapType)
 #define PyJudyIntSet_Check(op)       PyObject_TypeCheck(op, &PyJudyIntSetType)
-#define PyJudyIntSetIter_Check(op)   PyObject_TypeCheck(op, &PyJudyIntSetIterType)
 
 #endif
