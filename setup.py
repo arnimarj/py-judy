594dfe0a7f8088b05018b958a630e3879110beb3
arni <arni@dohop.com>
2014-07-28 11:41:32 +0000

    adding static judy build

--- setup.py
+++ setup.py
@@ -1,19 +1,42 @@
-import os, sys, commands
+import sys, commands
 from distutils.core import setup, Extension
 
-print 'INFO: building judy static library...'
+def build_judy():
+	print('INFO: building judy static library...')
 
-if sys.maxint == 2**63-1:
-	CFLAGS = '-O3 -fPIC -DJU_64BIT'
-elif sys.maxint == 2**31-1:
-	CFLAGS = '-O3 -fPIC'
-else:
-	sys.exit('bad sys.maxint')
+	# DRAGON WARNING: GCC 4.8.1 produces wrong code on all but -O0 optimization levels, falling back for now
+	if sys.maxint == 2**63-1:
+		CFLAGS = '-O0 -fPIC -DJU_64BIT'
+	elif sys.maxint == 2**31-1:
+		CFLAGS = '-O0 -fPIC'
+	else:
+		sys.exit('bad sys.maxint')
 
-a, b = commands.getstatusoutput('(cd judy/src; COPT=\'%s\' sh ./sh_build)' % (CFLAGS,))
+	a, b = commands.getstatusoutput('(cd judy-1.0.5/src; COPT=\'%s\' sh ./sh_build)' % (CFLAGS,))
 
-if a != 0:
-	sys.exit(b)
+	if a != 0:
+		sys.exit(b)
+
+build_judy()
+
+extra_link_args = ['-L./judy/src', '-Bstatic', '-lJudy', '-Bdynamic', '-lm']
+extra_compile_args = [
+	'-I./judy/src',
+	'-I./include',
+	'-I/usr/local/include',
+	'-Wall',
+	'-Wno-strict-prototypes',
+	'-g',
+	'-D_GNU_SOURCE',
+	'-O2',
+	'-DNDEBUG',
+#	'-fno-omit-frame-pointer',
+#	'-pg',
+#	'-fno-inline-functions',
+#	'-fno-inline-functions-called-once',
+#	'-fno-optimize-sibling-calls',
+#	'-fno-inline'
+]
 
 setup(
 	name = 'judy',
@@ -53,8 +76,8 @@ setup(
 				'utils.c'
 			],
 
-			extra_compile_args = ['-pedantic', '-std=c99', '-Wall', '-Wno-strict-prototypes', '-g', '-D_GNU_SOURCE', '-O2'],
-			extra_link_args = ['-L./judy/src', '-Bstatic', '-lJudy']
+			extra_compile_args = extra_compile_args,
+			extra_link_args = extra_link_args
 		)
 	]
 )
