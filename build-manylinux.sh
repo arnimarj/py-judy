c16df1eb08f6b4db97783a4fc92d5aaa785f9f4a
arnimarj <arnimarj@gmail.com>
2019-12-05 21:41:38 +0000

    lets try this

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -4,7 +4,7 @@ set -e
 
 mkdir -p ./wheelhouse
 
-docker run --rm -e PLAT=manylinux2014_x86_64 -v "$(pwd):/io" quay.io/pypa/manylinux2014_x86_64    /io/travis/build-wheels.sh
+docker run --rm -e PLAT=manylinux2010_x86_64 -v "$(pwd):/io" quay.io/pypa/manylinux2010_x86_64    /io/travis/build-wheels.sh
 docker run --rm -e PLAT=manylinux2010_x86_64 -v "$(pwd):/io" pypywheels/manylinux2010-pypy_x86_64 /io/travis/build-wheels.sh
 
 ls -l ./wheelhouse
--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
 test -n "$(shopt -s nullglob; echo /opt/python/*pypy3*)"
-is_mypy=$?
+is_pypy=$?
 
 set -ex
 
 mkdir -p ./wheels
 
+_clean() {
+	rm -rf /io/build
+	rm -rf /io/dist
+	rm -rf /io/judy-1.0.5/src/libJudy.a
+}
+
 for PYBIN in /opt/python/*/bin; do
 	# pypy, but only pypy3
-	if [ $is_mypy -eq 0 ]; then
+	if [ $is_pypy -eq 0 ]; then
 		if [[ $PYBIN == *"pypy3"* ]]; then
-
+			_clean;
+			"${PYBIN}/pip" install -U pip
 			"${PYBIN}/pip" wheel /io/ -w wheels/
 		fi
 	# cpython
 	else
 		if [[ $PYBIN =~ cp35|cp36|cp37|cp38 ]]; then
-
+			_clean;
+			"${PYBIN}/pip" install -U pip
 			"${PYBIN}/pip" wheel /io/ -w wheels/
 		fi
 	fi
