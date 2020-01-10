20dafb08d6ad750d6b92a15b8c6eb90dcc0f2c5c
arnimarj <arnimarj@gmail.com>
2020-01-10 14:27:55 +0000

    support manylinux 2014

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -4,7 +4,9 @@ set -e
 
 mkdir -p ./wheelhouse
 
-docker run --rm -e PLAT=manylinux2010_x86_64 -v "$(pwd):/io" quay.io/pypa/manylinux2010_x86_64    /io/travis/build-wheels.sh
-docker run --rm -e PLAT=manylinux2010_x86_64 -v "$(pwd):/io" pypywheels/manylinux2010-pypy_x86_64 /io/travis/build-wheels.sh
+docker run --rm -e PLAT=manylinux2010_x86_64 -v "$(pwd):/io" quay.io/pypa/manylinux2010_x86_64:latest    /io/travis/build-wheels.sh
+docker run --rm -e PLAT=manylinux2014_x86_64 -v "$(pwd):/io" quay.io/pypa/manylinux2014_x86_64:latest    /io/travis/build-wheels.sh
+
+docker run --rm -e PLAT=manylinux2010_x86_64 -v "$(pwd):/io" pypywheels/manylinux2010-pypy_x86_64:latest /io/travis/build-wheels.sh
 
 ls -l ./wheelhouse
