c1ed260046d70aaf8175f4067b9f56162d34f74a
arnimarj <arnimarj@gmail.com>
2020-05-13 11:45:01 +0000

    wip

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -10,3 +10,6 @@ docker run --rm -e PLAT=manylinux2014_x86_64 -v "$(pwd):/io" quay.io/pypa/manyli
 docker run --rm -e PLAT=manylinux2010_x86_64 -v "$(pwd):/io" pypywheels/manylinux2010-pypy_x86_64:latest /io/travis/build-wheels.sh
 
 ls -l ./wheelhouse
+
+# make sure we clean up ./build since it gets root permision
+sudo rm -rf ./build/
