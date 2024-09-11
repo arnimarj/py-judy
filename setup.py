4797b13559f6499dc9d6758fc065638448e6409c
arnimarj <arnimarj@gmail.com>
2024-09-11 20:15:47 +0000

    again

--- setup.py
+++ setup.py
@@ -1,50 +1,5 @@
-import os
-import subprocess
-import sys
-
 from setuptools import Extension, setup
 
-
-def build_judy():
-	print('INFO: building judy static library...')
-
-	CC = os.environ.get('CC', 'cc')
-
-	is_clang = False
-	is_gcc_46 = False
-
-	# test if CC is clang/gcc-4.6
-	exitcode, output = subprocess.getstatusoutput('%s -v' % (CC,))
-
-	if exitcode != 0:
-		sys.exit(output)
-
-	if 'clang' in output:
-		print('INFO: compiler is Clang')
-		is_clang = True
-	elif 'gcc version 4.6' in output:
-		print('INFO: compiler is GCC 4.6')
-		is_gcc_46 = True
-
-	# adding last two flags because of compiler and/or code bugs
-	# see http://sourceforge.net/p/judy/mailman/message/32417284/
-	assert sys.maxsize == (2**63 - 1)
-
-	if is_clang or is_gcc_46:
-		CFLAGS = '-DJU_64BIT -O0 -fPIC -fno-strict-aliasing'
-	else:
-		CFLAGS = '-DJU_64BIT -O0 -fPIC -fno-strict-aliasing -fno-aggressive-loop-optimizations'
-
-	exitcode, output = subprocess.getstatusoutput('(cd judy-1.0.5/src; CC=\'%s\' COPT=\'%s\' sh ./sh_build)' % (CC, CFLAGS))
-
-	if exitcode != 0:
-		sys.exit(output)
-
-	print(output)
-
-
-if not os.path.isfile('./judy-1.0.5/src/libJudy.a'):
-	build_judy()
 
 extra_link_args = ['-L./judy-1.0.5/src', '-Bstatic', '-lJudy', '-Bdynamic', '-lm']
 extra_compile_args = [
