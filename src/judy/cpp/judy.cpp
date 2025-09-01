d16944cbb89cc0919981e3ffa5ca464a29d32f22
arnimarj <arnimarj@gmail.com>
2025-09-01 16:53:55 +0000

    wip

--- src/judy/cpp/judy.cpp
+++ src/judy/cpp/judy.cpp
@@ -8,7 +8,7 @@
 
 #include "judy_int_set.h"
 #include "judy_int_int_mapping.h"
-￼
+
 namespace nb = nanobind;
 using namespace nb::literals;
 
@@ -18,14 +18,14 @@ NB_MODULE(_judy_nb, m) {
         .def(nb::init<>())
         .def("__contains__", &JudyIntIntMap::Contains)
         .def("__contains__", [](const JudyIntIntMap&, std::optional<nb::handle>) { return false; }, nb::arg("index").none())
-        .def("__len__", &JudyIntIntMap::size);
+        .def("__len__", &JudyIntIntMap::size)
         .def("clear", &JudyIntIntMap::Clear)
         .def("__getitem__", &JudyIntIntMap::GetItem)
         .def("__getitem__", [](const JudyIntIntMap&, std::optional<nb::handle>) { throw nb::key_error(); }, nb::arg("index").none())
         .def("__setitem__", &JudyIntIntMap::SetItem)
         .def("__str__", &JudyIntIntMap::ToString)
         .def("__repr__", &JudyIntIntMap::ToString)
-        .def("__sizeof__", &JudyIntIntMap::size_of);
+        .def("__sizeof__", &JudyIntIntMap::size_of)
         .def("get", &JudyIntIntMap::Get, nb::arg("index"), nb::arg("default").none() = nb::none())
         .def("pop", &JudyIntIntMap::Pop, nb::arg("index"))
         .def("pop", &JudyIntIntMap::PopDefault, nb::arg("index"), nb::arg("default").none() = nb::none())
