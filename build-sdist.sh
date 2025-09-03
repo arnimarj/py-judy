59ce74c66416bd22bd80977c678664233035beaf
arnimarj <arnimarj@gmail.com>
2025-09-03 09:50:45 +0000

    wip

--- build-sdist.sh
+++ build-sdist.sh
@@ -4,13 +4,11 @@ set -xe
 
 mkdir -p ./wheelhouse
 
-# pip install pipx
+pip install pipx
 pipx run build --sdist --outdir ./wheelhouse .
 
 # test it
 pip install -U pip virtualenv
 python -m virtualenv venv
 
-cmake -S . -B build
-cmake --build build
 ./venv/bin/pip install ./wheelhouse/*.gz
